7dac07f803270174b3cd3407cba2435bdf28c859
Dain Nilsson <dain@yubico.com>
2015-02-13 17:00:26 +0100

    Initial skeleton.

---
+++ pivtool/main.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2014 Yubico AB
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Additional permission under GNU GPL version 3 section 7
+#
+# If you modify this program, or any covered work, by linking or
+# combining it with the OpenSSL project's OpenSSL library (or a
+# modified version of that library), containing parts covered by the
+# terms of the OpenSSL or SSLeay licenses, We grant you additional
+# permission to convey the resulting work. Corresponding Source for a
+# non-source form of such a combination shall include the source code
+# for the parts of OpenSSL used as well as that of the covered work.
+
+import os
+import sys
+import argparse
+import pivtool.qt_resources
+from PySide import QtGui, QtCore
+from pivtool.view.main import MainWindow
+from pivtool import __version__ as version, messages as m
+
+
+if getattr(sys, 'frozen', False):
+    # we are running in a PyInstaller bundle
+    basedir = sys._MEIPASS
+else:
+    # we are running in a normal Python environment
+    basedir = os.path.dirname(__file__)
+
+# Font fixes for OSX
+if sys.platform == 'darwin':
+    from platform import mac_ver
+    mac_version = tuple(mac_ver()[0].split('.'))
+    if (10, 9) <= mac_version < (10, 10):  # Mavericks
+        QtGui.QFont.insertSubstitution(".Lucida Grande UI", "Lucida Grande")
+    if (10, 10) <= mac_version:  # Yosemite
+        QtGui.QFont.insertSubstitution(".Helvetica Neue DeskInterface", "Helvetica Neue")
+
+
+class PivtoolApplication(QtGui.QApplication):
+
+    def __init__(self, argv):
+        super(PivtoolApplication, self).__init__(argv)
+
+        self._set_basedir()
+
+        m._translate(self)
+
+        QtCore.QCoreApplication.setOrganizationName(m.organization)
+        QtCore.QCoreApplication.setOrganizationDomain(m.domain)
+        QtCore.QCoreApplication.setApplicationName(m.app_name)
+
+        args = self._parse_args()
+
+        self.window = self._create_window()
+
+
+    def _parse_args(self):
+        parser = argparse.ArgumentParser(description="Yubico PIV tool",
+                                         add_help=True)
+        return parser.parse_args()
+
+    def _set_basedir(self):
+        if getattr(sys, 'frozen', False):
+            # we are running in a PyInstaller bundle
+            self.basedir = sys._MEIPASS
+        else:
+            # we are running in a normal Python environment
+            self.basedir = os.path.dirname(__file__)
+
+    def _create_window(self):
+        window = MainWindow()
+        window.setWindowTitle(m.win_title_1 % version)
+        window.setWindowIcon(QtGui.QIcon(':/pivtool.png'))
+        window.show()
+        window.raise_()
+        return window
+
+
+def main():
+    app = PivtoolApplication(sys.argv)
+    status = app.exec_()
+    app.deleteLater()
+    sys.exit(status)
---
+++ setup.py
@@ -0,0 +1,70 @@
+# Copyright (c) 2014 Yubico AB
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Additional permission under GNU GPL version 3 section 7
+#
+# If you modify this program, or any covered work, by linking or
+# combining it with the OpenSSL project's OpenSSL library (or a
+# modified version of that library), containing parts covered by the
+# terms of the OpenSSL or SSLeay licenses, We grant you additional
+# permission to convey the resulting work. Corresponding Source for a
+# non-source form of such a combination shall include the source code
+# for the parts of OpenSSL used as well as that of the covered work.
+
+from setuptools import setup
+from release import release
+from qt_resources import qt_resources, qt_sdist
+import re
+
+VERSION_PATTERN = re.compile(r"(?m)^__version__\s*=\s*['\"](.+)['\"]$")
+
+
+def get_version():
+    """Return the current version as defined by pivtool/__init__.py."""
+
+    with open('pivtool/__init__.py', 'r') as f:
+        match = VERSION_PATTERN.search(f.read())
+        return match.group(1)
+
+setup(
+    name='yubico-pivtool-gui',
+    version=get_version(),
+    author='Dain Nilsson',
+    author_email='dain@yubico.com',
+    maintainer='Yubico Open Source Maintainers',
+    maintainer_email='ossmaint@yubico.com',
+    url='https://github.com/Yubico/yubico-pivtool-gui',
+    description='Tool for managing your YubiKey NEO configuration.',
+    license='GPLv3+',
+    packages=['pivtool', 'pivtool.model', 'pivtool.view'],
+    scripts=['scripts/pivtool'],
+    setup_requires=['nose>=1.0'],
+    install_requires=['PySide', 'pycrypto'],
+    test_suite='nose.collector',
+    tests_require=[''],
+    cmdclass={'release': release, 'qt_resources': qt_resources,
+              'sdist': qt_sdist},
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: End Users/Desktop',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Utilities'
+    ]
+)
