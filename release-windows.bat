88917774d17891aed2afb41faacec10c04f49558
Emil Lundberg <emil@yubico.com>
2018-08-06 16:42:48 +0200

    Add release script for Windows

---
+++ release-windows.bat
@@ -0,0 +1,12 @@
+SET VERSION="%1"
+
+ECHO "Building release of version: %VERSION%"
+
+SET RELEASE_DIR=".\dist"
+
+SET "PATH=%PATH%;C:\Program Files (x86)\NSIS"
+
+signtool sign /fd SHA256 /t http://timestamp.verisign.com/scripts/timstamp.dll "%RELEASE_DIR%\YubiKey PIV Manager"\pivman.exe
+makensis -D"VERSION=%VERSION%" resources\win-installer.nsi
+signtool sign /fd SHA256 /t http://timestamp.verisign.com/scripts/timstamp.dll "%RELEASE_DIR%\yubikey-piv-manager-%VERSION%-win.exe"
+gpg --detach-sign "yubikey-piv-manager-%VERSION%-win.exe"
