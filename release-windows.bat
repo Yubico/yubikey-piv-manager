7e6d2341450ee97de78adda499d624147c284f1b
Emil Lundberg <emil@yubico.com>
2018-09-26 14:05:12 +0200

    Add signtool to PATH in release-windows.bat

--- release-windows.bat
+++ release-windows.bat
@@ -5,6 +5,7 @@ ECHO "Building release of version: %VERSION%"
 SET RELEASE_DIR=".\dist"
 
 SET "PATH=%PATH%;C:\Program Files (x86)\NSIS"
+SET "PATH=%PATH%;C:\Program Files (x86)\Common Files\Microsoft\Visual C++ for Python\9.0\WinSDK\Bin"
 
 signtool sign /fd SHA256 /t http://timestamp.verisign.com/scripts/timstamp.dll "%RELEASE_DIR%\YubiKey PIV Manager"\pivman.exe
 makensis -D"VERSION=%VERSION%" resources\win-installer.nsi
